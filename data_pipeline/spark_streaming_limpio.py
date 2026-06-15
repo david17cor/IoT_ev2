eddc5e7acbce638dbd76f899f5daff60d85c24ee
david17cor <david.17.cor@gmail.com>
2026-06-15 16:03:19 -0400

    "Bajando cantidad de registros por batch"

--- data_pipeline/spark_streaming_limpio.py
+++ data_pipeline/spark_streaming_limpio.py
@@ -70,7 +70,7 @@ kafka_stream = spark.readStream \
     .option("kafka.bootstrap.servers", "kafka_broker:9092") \
     .option("subscribe", "telemetria_sucia") \
     .option("startingOffsets", "earliest") \
-    .option("maxOffsetsPerTrigger", "1000") \
+    .option("maxOffsetsPerTrigger", "100") \
     .load()
 
 
